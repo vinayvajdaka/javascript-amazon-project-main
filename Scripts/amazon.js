b26d8418a3b057d3f5ed1da7999a7fb4ed61475d
vinay vajdaka <vinayvakdaka1608@gmail.com>
2025-05-03 09:54:29 +0530

    make the quantity interactive

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -26,7 +26,7 @@ products.forEach((product)=>{
                 </div>
 
                 <div class="product-quantity-container">
-                    <select>
+                    <select class="quantity-selector-${product.id}">
                     <option selected value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -63,11 +63,13 @@ document.querySelector(".products-grid").innerHTML=productHtml;
 document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
     button.addEventListener("click",()=>{
         const productId=button.dataset.productId;
+        const product_qty=document.querySelector(`.quantity-selector-${productId}`).value;
+        
         
         let matchingItem='';
 
         cart.forEach((item)=>{
-                
+
             if(productId===item.productId){
                 matchingItem=item;
             }
@@ -81,7 +83,7 @@ document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
         else{
             cart.push({
                 productId:productId,
-                quantity:1
+                quantity:product_qty
             });
         }
         
